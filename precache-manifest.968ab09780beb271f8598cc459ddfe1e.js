f67c431d1a1828e34127c7ceaa45e9f493c1bff3
chirag <developer8@indglobal-consulting.com>
2020-06-06 17:54:22 +0530

    init push for byjus interview

---
+++ precache-manifest.968ab09780beb271f8598cc459ddfe1e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27862c87fffdd5e0b2a359f8dbfbc45d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6468f1aec364a16bfa2",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "db740f326545dcd202fb",
+    "url": "/static/css/3.25b83785.chunk.css"
+  },
+  {
+    "revision": "bfc76eafc90fd08a3e79",
+    "url": "/static/css/4.e687bf5e.chunk.css"
+  },
+  {
+    "revision": "9e4133f428379bd5a263",
+    "url": "/static/css/main.db782279.chunk.css"
+  },
+  {
+    "revision": "d6468f1aec364a16bfa2",
+    "url": "/static/js/2.5f83e2be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5f83e2be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db740f326545dcd202fb",
+    "url": "/static/js/3.88c33782.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/static/js/3.88c33782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc76eafc90fd08a3e79",
+    "url": "/static/js/4.c1a795e5.chunk.js"
+  },
+  {
+    "revision": "9e4133f428379bd5a263",
+    "url": "/static/js/main.7b93906d.chunk.js"
+  },
+  {
+    "revision": "fbbe25de29f1ba3eaf6d",
+    "url": "/static/js/runtime-main.3745704f.js"
+  },
+  {
+    "revision": "c4620399c21c9814ad780750762ab4fa",
+    "url": "/static/media/icon.c4620399.svg"
+  }
+]);
